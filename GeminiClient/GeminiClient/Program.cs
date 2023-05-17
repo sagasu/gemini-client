fbdadf0a16418b4cf08c90eadcbfb17ed95a6161
Mateusz Kopij <sagasu@gmail.com>
2023-05-17 09:39:31 +0100

    POC getting data from Gemini

---
+++ GeminiClient/GeminiClient/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Websocket.Client;
+
+Console.WriteLine("Hello, World!");
+
+Initialize();
+
+ void Initialize()
+{
+    Console.CursorVisible = false;
+    try
+    {
+        var exitEvent = new ManualResetEvent(false);
+        var url = new Uri("wss://api.gemini.com/v1/marketdata/BTCUSD");
+
+        using var client = new WebsocketClient(url);
+        
+        client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+        client.ReconnectionHappened.Subscribe(info =>
+        {
+            Console.WriteLine("Reconnection happened, type: " + info.Type);
+        });
+        client.MessageReceived.Subscribe(msg =>
+        {
+            Console.WriteLine("Message received: " + msg);
+            if (msg.ToString().ToLower() == "connected")
+            {
+                var data = "";
+                client.Send(data);
+            }
+        });
+
+        client.Start();
+        exitEvent.WaitOne();
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("ERROR: " + ex.ToString());
+    }
+    Console.ReadKey();
+}
